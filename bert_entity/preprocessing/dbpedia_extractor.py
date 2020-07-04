a424bb8dda7a7d19fc054adb4ad20ad8534638c7
TonyLorenz <38682194+TonyLorenz@users.noreply.github.com>
2020-07-04 18:45:09 +0200

    Update dbpedia_extractor.py

--- bert_entity/preprocessing/dbpedia_extractor.py
+++ bert_entity/preprocessing/dbpedia_extractor.py
@@ -47,29 +47,29 @@ class Wikiextractor(PipelineJob):
               #  print(data[i]['internal links'])
 
 
-                while i<len(data):
-                  internal_links_new = []
-                  offset_list = []
-                  item = data[i]['internal links'].split(', ')
-                  for word in item:
-                    word = word.replace('_',' ')
-                    internal_links_new.append((word, word))
-              #      print(word)
-                    x = data[i]['text'].find(word)
-                    if x>0:
-                      offset_list.append((x, x + len(word)))
-                    else:
-                      offset_list.append(x)
-                  data[i]['internal links'] = dict(zip(offset_list, internal_links_new))
-                  del data[i]['internal links'][-1]
-              #    data[i]['internal links'] = sorted(data[i]['internal links'].items())
-                  data[i]['internal links'] = base64.b64encode(pickle.dumps(data[i]['internal links'])).decode('utf-8')
-                  print(data[i]['internal links'])
-                  i+=1
+              while i<len(data):
+                internal_links_new = []
+                offset_list = []
+                item = data[i]['internal links'].split(', ')
+                for word in item:
+                  word = word.replace('_',' ')
+                  internal_links_new.append((word, word))
+            #      print(word)
+                  x = data[i]['text'].find(word)
+                  if x>0:
+                    offset_list.append((x, x + len(word)))
+                  else:
+                    offset_list.append(x)
+                data[i]['internal links'] = dict(zip(offset_list, internal_links_new))
+                del data[i]['internal links'][-1]
+            #    data[i]['internal links'] = sorted(data[i]['internal links'].items())
+                data[i]['internal links'] = base64.b64encode(pickle.dumps(data[i]['internal links'])).decode('utf-8')
+                print(data[i]['internal links'])
+                i+=1
               #    base64.b64encode(pickle.dumps(self.internal_links)).decode('utf-8')
 
               #    print(data)
-              new_file = f"db_file_{counter}.txt)
+              new_file = f"db_file_{counter}.txt"
               os.makedirs(f"data/versions/{self.opts.data_version_name}/wikiextractor_out/{self.opts.wiki_lang_version}")
               f = open(f"data/versions/{self.opts.data_version_name}/wikiextractor_out/{self.opts.wiki_lang_version}/{new_file}","w")
               f.write( str(data))
