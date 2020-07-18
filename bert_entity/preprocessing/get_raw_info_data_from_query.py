816cc2ca494e07795468ba78fe6e5d7826ba6973
TonyLorenz <38682194+TonyLorenz@users.noreply.github.com>
2020-07-18 22:27:29 +0200

    Update get_raw_info_data_from_query.py

--- bert_entity/preprocessing/get_raw_info_data_from_query.py
+++ bert_entity/preprocessing/get_raw_info_data_from_query.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import random
 
-data = pd.read_csv("info_query_out")
+data = pd.read_csv("query_out")
 #data = data.head(5000)
 
 data.to_csv("data_info.csv", index= False, header= True)
