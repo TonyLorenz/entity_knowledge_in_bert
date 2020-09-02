d044fdca3018f75737275ff62f37feef095fc483
TonyLorenz <38682194+TonyLorenz@users.noreply.github.com>
2020-09-02 17:23:35 +0200

    Update get_raw_info_data_from_query_dummy.py

--- bert_entity/preprocessing/get_raw_info_data_from_query_dummy.py
+++ bert_entity/preprocessing/get_raw_info_data_from_query_dummy.py
@@ -2,9 +2,9 @@ import pandas as pd
 import re
 import random
 
-    """
-    Make csv from queried info data.
-    """
+
+#Make csv from queried info data.
+
   
 data = pd.read_csv("query_out")
 data = data.head(1000000)
