a543934a25a39ad9f3591965225da87333625894
TonyLorenz <38682194+TonyLorenz@users.noreply.github.com>
2020-09-02 11:20:05 +0200

    Update get_links_data_from_ttl_links_file.py

--- bert_entity/preprocessing/get_links_data_from_ttl_links_file.py
+++ bert_entity/preprocessing/get_links_data_from_ttl_links_file.py
@@ -1,6 +1,10 @@
 import re
 import pandas as pd
 
+    """
+    Read through page_links_en.ttl file that contains all internal links. Then puts URI and internal link into individual strings.
+    """
+  
 fp = open('page_links_en.ttl', 'r')
 print("fp = open('page_links_en.ttl', 'r') done")
 
@@ -28,6 +32,10 @@ url = []
 
 i =0
 
+    """
+    Split and edit each line to get URI and internal links individually as strings.
+    """
+  
 while i<len(links_df['internal_links']):
   item = str(links_df['internal_links'][i])
   item = item.split(' ')
